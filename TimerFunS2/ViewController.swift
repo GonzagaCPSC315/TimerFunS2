00d0872a6465bd8058a28e3eb58dac6fb230f158
Gina Sprint <sprint@gonzaga.edu>
2020-10-05 11:30:58 -0700

    Initial Commit

---
+++ TimerFunS2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimerFunS2
+//
+//  Created by Gina Sprint on 10/5/20.
+//  Copyright © 2020 Gina Sprint. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
